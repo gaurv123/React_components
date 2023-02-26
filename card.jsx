4072a72f7eecdc12dc40bd255b2fa35ea793b7c6
thakurgaurav273 <91819071+gaurv123@users.noreply.github.com>
2023-02-26 11:55:26 +0530

    added a card component

---
+++ card.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import Avatar from "./avatar";
+function Card(props){
+    return( 
+        
+            <div className="card">
+                <div className="upper">
+                    <h4>{props.name}</h4>
+                    <Avatar src={props.src}/>
+                    
+                </div>
+                <div className="lower">
+                    <span>{props.mob}</span> <br />
+                    <span>{props.emailid}</span>
+                </div>
+            </div>
+       
+    )
+   
+}
+export default Card;
